602dc1752f5c2b4e814c8c18f3c571d827001c3c
Daniel Vermeulen <34882@ma-web.nl>
2024-02-02 18:00:24 +0100

    fixed js bugs to get burger menu to be interactive with the header. added start to burger menu layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,28 @@
 const burgermenu = document.getElementById("burger");
 const burgerbars = document.getElementsByClassName("burger__bar");
+const whiteBar = document.getElementById("whiteBar");
+const headerBar = document.getElementById("headerBar");
+const headerContent = document.querySelector('.header__content');
+
+burgermenu.addEventListener("click", function () {
+    if (whiteBar.style.width === "25%") {
+        whiteBar.style.width = "0";
+        headerBar.style.width = "0";
+        headerContent.style.transform = "translateX(0%)";
+        document.body.style.overflow = 'hidden';
+        for (let i = 0; i < burgerbars.length; i++) {
+            burgerbars[i].style.backgroundColor = "white";
+        }
+    } else {
+        whiteBar.style.width = "25%";
+        headerBar.style.width = "25%";
+        headerContent.style.transform = "translateX(25%)";
+        document.body.style.overflow = 'hidden';
+        for (let i = 0; i < burgerbars.length; i++) {
+            burgerbars[i].style.backgroundColor = "rgb(202, 202, 202)";
+        }
+    }
+});
 
 burgermenu.addEventListener("mouseover", function () {
     for (let i = 0; i < burgerbars.length; i++) {
