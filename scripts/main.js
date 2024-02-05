d74a5a694615bfde16cb5e8d5962e4d05a96e996
Daniel Vermeulen <34882@ma-web.nl>
2024-02-05 15:30:43 +0100

    Lightsaber scrollbar for the most part, scrollbar css not developed enough to make it perfect.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 const burgermenu = document.getElementById("burger");
 const burgerbars = document.getElementsByClassName("burger__bar");
 const whiteBar = document.getElementById("whiteBar");
+const mainpage = document.getElementById("main");
 
 burgermenu.addEventListener("click", function () {
     if (whiteBar.style.width === "25%") {
         whiteBar.style.width = "0";
         burgermenu.style.left = "0"; // Move the burger menu back to its original position
+        mainpage.style.marginLeft = "5%"
+        mainpage.style.width = "80vw";
         document.body.style.overflow = 'hidden';
         for (let i = 0; i < burgerbars.length; i++) {
             burgerbars[i].style.backgroundColor = "white";
@@ -13,6 +16,8 @@ burgermenu.addEventListener("click", function () {
     } else {
         whiteBar.style.width = "25%";
         burgermenu.style.left = "25%"; // Move the burger menu to the right
+        mainpage.style.marginLeft = "30%"
+        mainpage.style.width = "55vw";
         document.body.style.overflow = 'hidden';
         for (let i = 0; i < burgerbars.length; i++) {
             burgerbars[i].style.backgroundColor = "rgb(202, 202, 202)";
