d764f1bfc647af445581796b75ac25ef1d240c22
Daniel-Gman <Leinad.vermeulen@gmail.com>
2024-04-15 15:09:47 +0200

    Total reset of design

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,9 @@
-const burgermenu = document.getElementById("burger");
-const burgerbars = document.getElementsByClassName("burger__bar");
-const whiteBar = document.getElementById("whiteBar");
-const burgercontent = document.getElementById("burger__content");
-const burgerimage = document.getElementById("burger__image");
+const left = document.getElementById("left__side");
 
-burgermenu.addEventListener("click", function () {
-    if (whiteBar.style.width === "25%") {
-        whiteBar.style.width = "0";
-        burgermenu.style.left = "0"; // Move the burger menu back to its original position
-        burgercontent.style.opacity = "0";
-        burgerimage.style.opacity = "0"
-        document.body.style.overflow = 'hidden';
-        for (let i = 0; i < burgerbars.length; i++) {
-            burgerbars[i].style.backgroundColor = "white";
-        }
-    } else {
-        whiteBar.style.width = "25%";
-        burgermenu.style.left = "25%"; // Move the burger menu to the right
-        setTimeout(function () {
-            burgercontent.style.opacity = "1";
-            burgerimage.style.opacity = "1"
-        }, 200); // Adjust the delay time as needed
-        document.body.style.overflow = 'hidden';
-        for (let i = 0; i < burgerbars.length; i++) {
-            burgerbars[i].style.backgroundColor = "rgb(202, 202, 202)";
-        }
-    }
-});
+const handleMove = e => {
+    left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+}
 
-// Mouse Hover function from here:
-// __________________________________________________________>
+document.onmousemove = e => handleMove(e);
 
-burgermenu.addEventListener("mouseover", function () {
-    for (let i = 0; i < burgerbars.length; i++) {
-        burgerbars[i].style.transition = "background-color 0.3s ease"; // Add transition property
-        burgerbars[i].style.backgroundColor = "rgb(202, 202, 202)";
-    }
-});
-
-burgermenu.addEventListener("mouseout", function () {
-    for (let i = 0; i < burgerbars.length; i++) {
-        burgerbars[i].style.transition = "background-color 0.3s ease"; // Add transition property
-        // Reset to the original color
-        burgerbars[i].style.backgroundColor = 'white';
-    }
-});
+document.ontouchmove = e => handleMove(e.touches[0]);
