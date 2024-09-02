7de8ffd1a2f042584d22beb48f1b1c32c5594bfd
Daniel-Gman <Leinad.vermeulen@gmail.com>
2024-09-02 16:10:52 +0200

    Total revamp of portfolio

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
-const left = document.getElementById("left__side");
+const header = document.getElementById("header");
+const arrow = document.getElementById("arrow");
+const header__list = document.getElementById("header__li");
 
-const handleMove = e => {
-    left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+var arrow__bool = true;
+
+// Function to adjust header height based on screen width
+function adjustHeaderHeight() {
+    // Check if screen width is 460px or less
+    const isSmallScreen = window.matchMedia("(max-width: 460px)").matches;
+
+    if (arrow__bool === true) {
+        header.style.height = isSmallScreen ? "18rem" : "8rem";   // Adjust the header height based on screen width
+        header__list.style.opacity = "1";     // Make the header content visible with transition
+        arrow__bool = false;
+    } else {
+        header.style.height = "0";      // Get header back to original height
+        header__list.style.opacity = "0";     // Fade out the header content for smooth transitioning
+        arrow__bool = true;
+    }
 }
 
-document.onmousemove = e => handleMove(e);
-
-document.ontouchmove = e => handleMove(e.touches[0]);
+// Added event listener to the arrow
+arrow.addEventListener("click", adjustHeaderHeight);
